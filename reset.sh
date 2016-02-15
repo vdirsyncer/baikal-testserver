1cd9218479c60166d672c6ec33061022fc5db810
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-02-15 21:53:41 +0100

    Restore configs

--- reset.sh
+++ reset.sh
@@ -3,4 +3,6 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd "$DIR"
-cp config/db.sqlite baikal/Specific/db/db.sqlite
+cp config/db.sqlite baikal/Specific/db/
+cp config/config.php baikal/Specific/
+cp config/config.system.php baikal/Specific/
