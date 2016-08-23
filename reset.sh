a00e2d38eb92adc4dcbb39047cd6a4a7c0b0b4d3
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-08-23 16:04:03 +0200

    Remove duplicate DB from git repo

--- reset.sh
+++ reset.sh
@@ -3,6 +3,7 @@ set -e
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd "$DIR"
+mkdir -p baikal/Specific/db/
 cp config/db.sqlite baikal/Specific/db/
 cp config/config.php baikal/Specific/
 cp config/config.system.php baikal/Specific/
